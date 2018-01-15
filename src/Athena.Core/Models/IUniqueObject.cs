c66ebb022121b8add4ca3af2a6ee2d0cec94f53e
Nathan Lowe <techwiz96@gmail.com>
2018-01-15 00:03:25 -0500

    Generic repositories and unique objects

---
+++ src/Athena.Core/Models/IUniqueObject.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Athena.Core.Models
+{
+    public interface IUniqueObject<T> where T : IEquatable<T>
+    {
+        T Id { get; set; }
+    }
+}
---
+++ src/Athena.Core/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Athena.Core.Models;
+
+namespace Athena.Core.Repositories
+{
+    public interface IRepository<T, in TKey>
+        where TKey : IEquatable<TKey>
+        where T : class, IUniqueObject<TKey>
+    {
+        Task<T> GetAsync(TKey id);
+
+        Task<T> AddAsync(T obj);
+        Task<T> EditAsync(T obj);
+        Task<T> DeleteAsync(T obj);
+    }
+}
