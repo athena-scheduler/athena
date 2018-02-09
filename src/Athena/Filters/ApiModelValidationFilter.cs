e3906913f7e105c34e283f135ad18e1344d92967
Nathan Lowe <techwiz96@gmail.com>
2018-02-08 19:49:35 -0500

    Auto-Validate for api controllers

---
+++ src/Athena/Filters/ApiModelValidationFilter.cs
@@ -0,0 +1,27 @@
+﻿using Athena.Exceptions;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Athena.Filters
+{
+    public class ApiModelValidationFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var path = context.HttpContext.Request.Path;
+
+            // Only Validate automatically for API controllers
+            if (path.HasValue && path.Value.ToLowerInvariant().Contains("api/v"))
+            {
+                if (!context.ModelState.IsValid)
+                {
+                    throw new BadModelException(context.ModelState);
+                }
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // No Post Actions for this filter
+        }
+    }
+}
