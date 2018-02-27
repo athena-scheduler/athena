8a7f903cf0cd00ee4880dbf5279913a7451e9b55
Nathan Lowe <techwiz96@gmail.com>
2018-02-27 16:46:29 -0500

    Don't push on PR's and fix push task name

--- build/travis.sh
+++ build/travis.sh
@@ -6,8 +6,8 @@ SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 CAKE_TASK=Default
 
-if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    CAKE_TASK=Docker::Publish
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+    CAKE_TASK=Docker::Push
     docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 fi
 
