fe1243cbcf2fe07f69535fc1c9be9d58444cae45
Nathan Lowe <techwiz96@gmail.com>
2018-03-31 11:28:03 -0400

    Hotfix: Fix issues from offering refactor
    * Open the DB connection before attempting to create a transaction
    * Don't forget to pull back the offering ID when getting meetings for an offering
    * Importer doesn't need to link meetings to offering, it's implicit

--- src/Athena.Data/Extensions/DbConnectionExtensions.cs
+++ src/Athena.Data/Extensions/DbConnectionExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System.Data;
+using System.Data.Common;
 using System.Threading.Tasks;
+using System.Transactions;
 using Athena.Core.Exceptions;
 using Dapper;
 using Npgsql;
@@ -23,6 +25,25 @@ namespace Athena.Data.Extensions
 
                 throw;
             }
+        }
+        
+        /// <summary>
+        /// Create a transcation scope with Async Support and enlist the specified connection with the transaction
+        /// </summary>
+        /// <param name="db">The DB Connection to enlist</param>
+        /// <returns>A transaction scope</returns>
+        public static TransactionScope CreateAsyncTransactionScope(this IDbConnection db)
+        {
+            var result = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            if (db.State != ConnectionState.Open)
+            {
+                db.Open();
+            }
+            
+            (db as DbConnection)?.EnlistTransaction(Transaction.Current);
+
+            return result;
         }
     }
 }
